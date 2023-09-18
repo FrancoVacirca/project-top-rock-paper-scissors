5bee8dad058d8f4125b413175be36dc51516ecf5
Franco <131470535+FrancoVacirca@users.noreply.github.com>
2023-09-18 16:06:25 -0300

    1.0

--- main.js
+++ main.js
@@ -1,17 +1,59 @@
-//pc choses a random number
-const pcRandomNumber = Math.floor(Math.random() * 3) + 1;
+function getComputerChoice() {
+    const options = ['rock', 'paper', 'scissors'];
+    const randomIndex = Math.floor(Math.random() * options.length);
+    return options[randomIndex];
+}
 
-//get the pc random number and changes it to rock, paper, scissors
-function getComputerChoice(pcObject) {
-    if (pcRandomNumber === 1) {
-        return "Rock!"
-    } else if (pcRandomNumber === 2) {
-        return "Paper!"
+let playerPoints = (0);
+let computerPoints = (0);
+
+function playGame() {
+    const userChoice = prompt("Choose your weapon: Rock, Paper, Scissors").toLowerCase();
+    const computerChoice = getComputerChoice();
+
+    console.log(`You choose: ${userChoice}`);
+    console.log(`Computer choose: ${computerChoice}`);
+
+    if (userChoice === computerChoice) {
+        return "It's a tie!"     
+    } else if (
+        (userChoice === "rock" && computerChoice === "scissors") ||
+        (userChoice === "paper" && computerChoice === "rock") ||
+        (userChoice === "scissors" && computerChoice === "paper")
+    ) {
+        playerPoints++;
+        return ("You win!");  
     } else {
-        return "Scissors!"
+        computerPoints++
+        return ("You lose :(");
     }
 }
 
-console.log("The computer choice: ",getComputerChoice());
+let roundsPlayed = (1);
 
-//TODO store the pc object
+function game() {
+    while (roundsPlayed <= 5) {
+        const result = playGame()
+        console.log(result);
+        roundsPlayed++;
+    }
+    
+    console.log(`Player Points: ${playerPoints}`);
+    console.log(`Computer Points: ${computerPoints}`);
+
+    const gameResult = winner();
+    console.log(gameResult);
+}
+
+function winner() {
+    if (playerPoints > computerPoints) {
+        return "Player Wins!";
+    } else if (computerPoints > playerPoints) {
+        return "Computer Wins";
+    } else {
+        return "It's a tie!";
+    }
+}
+
+game();
+
